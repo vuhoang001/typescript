3da856867edc3e3ea3050486aa202394aa5bd846
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-03-06 23:15:26 +0700

    Create project

--- hello.ts
+++ hello.ts
@@ -1,3 +1,76 @@
-const hello = 'Xin chao! toi ten laasd hoang asdfasdf'
-console.log(hello)
-// asdfasdf
+
+// String 
+let a: string = "xin chao !"
+
+
+//object 
+
+const user: {
+    fullName: string,
+    email: String,
+    age: Number
+} = {
+    fullName: "Tran Vu Hoang",
+    email: "vuhoang250203@gmail.com",
+    age: 12
+}
+
+console.log(user)
+
+// 5.Interface 
+interface User {
+    fullName: String,
+    email: String,
+    age?: Number,
+    socials?: {
+        zalo: String,
+        facebook: String
+    }
+}
+
+let detail: User = {
+    fullName: "Tran Vu Hoang",
+    email: "Vuhoang250203@gmail.com",
+    age: 11,
+    socials: {
+        zalo: "zalo.com",
+        facebook: "facebook.com"
+    }
+}
+console.log(detail)
+
+// 6. Extend interface  
+interface Account {
+    fullName: String,
+    email: String,
+    phone: String,
+    password: String
+}
+
+interface AdminAccount extends Account {
+    roleID: String,
+    hotline: String
+}
+
+interface ClientAccount extends Account {
+    score: String,
+    average: Number
+}
+
+const admin: AdminAccount = {
+    fullName: "Le Van A",
+    email: "levana@gmail.com",
+    phone: "123345",
+    password: "hoang",
+    roleID: "123",
+    hotline: "190018123"
+}
+
+const client: ClientAccount = {
+    fullName: "Le Van A",
+    email: "levana@gmail.com",
+    phone: "123345",
+    password: '123',
+    score: 'asdfasdfzxkcjlhv',
+    average: 12
+}
--- js/hello.js
+++ js/hello.js
@@ -1,2 +1,33 @@
-const hello = 'Xin chao! toi ten laasd hoang asdfasdf';
-console.log(hello);
+let a = "xin chao !";
+const user = {
+    fullName: "Tran Vu Hoang",
+    email: "vuhoang250203@gmail.com",
+    age: 12
+};
+console.log(user);
+let detail = {
+    fullName: "Tran Vu Hoang",
+    email: "Vuhoang250203@gmail.com",
+    age: 11,
+    socials: {
+        zalo: "zalo.com",
+        facebook: "facebook.com"
+    }
+};
+console.log(detail);
+const admin = {
+    fullName: "Le Van A",
+    email: "levana@gmail.com",
+    phone: "123345",
+    password: "hoang",
+    roleID: "123",
+    hotline: "190018123"
+};
+const client = {
+    fullName: "Le Van A",
+    email: "levana@gmail.com",
+    phone: "123345",
+    password: '123',
+    score: 'asdfasdfzxkcjlhv',
+    average: 12
+};
