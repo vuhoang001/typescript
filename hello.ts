b98959a70bf59075c1cc71fec0728dbf3b00b080
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-03-08 14:12:14 +0700

    END 36

--- hello.ts
+++ hello.ts
@@ -126,14 +126,122 @@
 //         + Số lượng phần tử trong tuple là cố định
 //         + Types của các phần tử trong mảng được chỉ định và không cần giống nhau
 
-let inforUser: [String, Number, String]
+// let inforUser: [String, Number, String]
 
-inforUser = ['Nguyen Van A', 12, "Tran Vu Hoang"]
+// inforUser = ['Nguyen Van A', 12, "Tran Vu Hoang"]
 
 
-let clock: [Number, Number, Number]
+// let clock: [Number, Number, Number]
 
-clock = [12, 59, 23]
+// clock = [12, 59, 23]
 
-const [hours, minutes, seconds] = clock
-console.log(`Bây giờ là ${hours} giờ ${minutes} phút ${seconds} giây`)
+// const [hours, minutes, seconds] = clock
+// console.log(`Bây giờ là ${hours} giờ ${minutes} phút ${seconds} giây`)
+
+// 9. Kiểu readonly
+//     + Từ khóa readonly được sử dụng để chỉ định rằng một biến hoặc thuộc tính không thể được gán lại sau khi nó đã được khởi tạo
+
+// interface User {
+//     readonly fullName: String,
+//     email: String,
+//     age?: Number
+// }
+
+// const user1: User = {
+//     fullName: "Tran Vu Hoang",
+//     email: "hoang250203@gmail.com",
+//     age: 12
+// }
+
+// console.log(user1)
+
+// // 10. Function
+// function sum(a: number, b: number): number {
+//     return (a + b);
+// }
+// let result = sum(10, 20)
+// console.log(result)
+
+
+// const sum1 = (a: number, b: number): number => {
+//     return a + b
+// }
+
+// // 11.Default parameters
+// const sum = (a: number = 0, b: number = 0): number => {
+//     return a + b;
+// }
+
+// // 12.Rest parameters
+// const sum = (...numbers: number[]) => {
+//     const total = numbers.reduce((sum, item) => sum + item, 0)
+//     return total
+// }
+
+// const res = sum(10, 20, 30, 40, 50)
+// console.log(res)
+
+// 13. Enum
+//     + Liệt kê một nhóm các giá trị constant
+// SAMPLE 1:
+// enum Status {
+//     INITAL, // 0
+//     ACTIVE, // 1
+//     INACTIVE // 2
+// }
+// const statusCurrent: number = 2
+
+// switch (statusCurrent) {
+//     case Status.INITAL:
+//         console.log("Đang khởi động")
+//     case Status.ACTIVE:
+//         console.log("Đang hoat động")
+//         break;
+//     case Status.INACTIVE:
+//         console.log("Dung khởi động")
+//         break;
+//     default:
+//         break;
+// }
+
+
+// // SAMPLE 2:
+// enum Status {
+//     INITAL = "inital", // 0
+//     ACTIVE = 'active', // 1
+//     INACTIVE = 'inactive'// 2
+// }
+// const statusCurrent: string = "active"
+
+// switch (statusCurrent) {
+//     case Status.INITAL:
+//         console.log("Đang khởi động")
+//     case Status.ACTIVE:
+//         console.log("Đang hoat động")
+//         break;
+//     case Status.INACTIVE:
+//         console.log("Dung khởi động")
+//         break;
+//     default:
+//         break;
+// }
+
+// // 14. Kiểu any
+// // + Giúp linh họat thay đổi các kiểu dữ liệu của 1 biến
+// let a: any = 20
+// a = 'Le Van A'
+
+// console.log(a)
+
+// 15. Unknow
+// + Giúp thay đổi linh hoạt kiểu dữ liệu của 1 biến
+// + Nhưng an toàn hơn
+// + Kiểu any KHÔNG yêu cầu kiểm tra dữ liệu trước khi sử dụng giá trị -> Khi gặp lỗi không hiển thị thông báo
+// + Kiểu unknow CÓ yêu cầu kiểm tra dữ liệu trước khi sử dụng giá trị -> Khi gặp lỗi có hiển thị thông báo
+
+// let a: unknown;
+// a = 12
+// a = "le Van A"
+// console.log(a)
+
+
--- js/hello.js
+++
@@ -1,6 +0,0 @@
-let inforUser;
-inforUser = ['Nguyen Van A', 12, "Tran Vu Hoang"];
-let clock;
-clock = [12, 59, 23];
-const [hours, minutes, seconds] = clock;
-console.log(`Bây giờ là ${hours} giờ ${minutes} phút ${seconds} giây`);
